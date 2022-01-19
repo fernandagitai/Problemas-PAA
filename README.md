34a7772010dfced74d0c70b7d0302c5c9db92bb1
Fernanda Herculano <fernandaherculanom@gmail.com>
2022-01-19 01:22:31 -0300

    Adicionando o problema da festa

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Resolver o problema da Torre de Hanói quando se dispõe de 4 torres, em vez de 
 
 ## AGENDAMENTO
 Considere o problema de agendamento de intervalos. Nós temos um conjunto de atividades {1,2, ... n}; cada atividade i possui um intervalo de tempo a partir de si e termina em fi. Um agendamento — conjunto de atividades — é dito compatível, se nenhuma atividade se sobrepõem no tempo. O objetivo é determinar um agendamento compatível com o maior número possível de atividades. Projete um algoritmo para este problema.
+
+## CONVIDADOS DE FESTA
+Alice quer dar uma festa e está decidindo quem chamar. Ela tem n pessoas as quais escolher, e ela fez uma lista de quais pares dessas pessoas conhecem uma a outra. Ela quer selecionar o maior número de pessoas possível, sujeito a duas restrições: na festa, cada pessoa deve ter pelo menos outras cinco pessoas que ela conhece e outras cinco pessoas que ela não conhece. Forneça um algoritmo eficiente que tome como entrada a lista das n pessoas e a lista de pares de quem conhece quem e calcule a melhor escolha de convidados para a festa. Dê o tempo de execução em termos de n.
---
+++ src/festa.py
@@ -0,0 +1,58 @@
+
+
+def escolher_convidados(pares):
+    total_pessoas = len(pares)
+    convidados = [1]*total_pessoas
+
+    for pessoa in pares:
+        conhecidos = len(pares[pessoa])
+        desconhecidos = total_pessoas - conhecidos
+        
+        if conhecidos < 5 or desconhecidos < 5:
+            identificador = int(pessoa)
+            convidados[identificador] = 0
+    
+    return convidados
+
+
+def entrada():
+    print("\nDigite apenas numeros...\n")
+
+    qtd_pessoas = int(input("Digite quantas pessoas serao analisadas: "))
+    print("O identificador ira comecar no ZERO.\n")
+    pares = {}
+
+    for i in range(qtd_pessoas):
+        identificador = str(i)
+        pares[identificador] = []
+
+    while True:
+        linha_par = input("Digite <id1> <id2> dos pares ou '0' para finalizar: ")
+        if linha_par == "0": break
+
+        linha_par = linha_par.split()
+        pares[linha_par[0]].append(linha_par[-1])
+        pares[linha_par[-1]].append(linha_par[0])
+
+    return pares
+
+def printar_convidados(convidados):
+    saida = "Convidados: "
+
+    for i in range(len(convidados)):
+
+        if convidados[i]:
+            if saida != "Convidados: ":
+                saida += ", "
+            saida += str(i)
+
+    print("\n" + saida + "\n")
+
+def main():
+    pares = entrada()
+    convidados = escolher_convidados(pares)
+    printar_convidados(convidados)
+
+
+if __name__ == "__main__":
+    main()
